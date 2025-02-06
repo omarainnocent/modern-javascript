529a48ea56051cc13b4169bc8c71b0cfa43368fa
im <omarainnocent644@gmail.com>
2025-02-06 16:24:45 +0300

    challenge

--- test/challege.js
+++ test/challege.js
@@ -1,27 +1,109 @@
+// let markMass2 = 95;
+// let markHeight2 = 1.88;
+// let johnMass2 = 85;
+// let johnHeight2 = 1.76;
+
+
+// let markBMI2 = markMass2 / markHeight2 ** 2;
+// let johnBMI2 = johnMass2 / johnHeight2 ** 2;
+
+// let markHigherBMI2 = markBMI2 > johnBMI2;
+
+// console.log(johnBMI2);
+// console.log(markHigherBMI2);
+// console.log( johnBMI2);
+
+// // challenge 2
+// if (markBMI2 > johnBMI2) {
+//     console.log(`Mark's BMI is higher than John's !`);
+// } else if (johnBMI2 > markBMI2) {
+//     console.log(`John's BMI is higher than Mark's !`);
+// } else {
+//     console.log(`Mark and John's BMI are the same`);
+// }
+
+// challenge 3
+ const averageDolphins = (97 + 112 + 101) / 3;
+ const averageKoalas = (109 + 95 + 106) / 3;
+ 
+ 
+ console.log(averageDolphins);
+ console.log(averageKoalas);
+
+if (averageKoalas > averageDolphins && averageDolphins >= 100) {
+    console.log('Dolphins wins the trophy');
+
+}
+else if (averageKoalas > averageDolphins && averageKoalas >= 100) {
+    console.log('Koalas wins the trophy');
+}
+ else if (averageDolphins === averageKoalas && averageDolphins >= 100 && averageKoalas >= 100) {
+    console.log('it is a draw');
+ } 
+ else {
+    console.log('no team wins the trophy')
+ }
+
+
+//  Coding Challenge #4
+// const bill = 275
+// const tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+// const total = (bill + tip)
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${total}`)
 
 
 
-let markMass2 = 95;
-let markHeight2 = 1.88;
-let johnMass2 = 85;
-let johnHeight2 = 1.76;
 
 
-let markBMI2 = markMass2 / markHeight2 ** 2;
-let johnBMI2 = johnMass2 / johnHeight2 ** 2;
 
-let markHigherBMI2 = markBMI2 > johnBMI2;
 
-console.log(johnBMI2);
-console.log(markHigherBMI2);
-console.log( johnBMI2);
 
-// challenge 2
-if (markBMI2 > johnBMI2) {
-    console.log(`Mark's BMI is higher than John's !`);
-} else if (johnBMI2 > markBMI2) {
-    console.log(`John's BMI is higher than Mark's !`);
-} else {
-    console.log(`Mark and John's BMI are the same`);
+
+
+
+
+
+
+
+// CHALLENGE 1
+const calcAverage = (a, b, c) => {
+    return (a + b + c) / 3
 }
 
+
+const avgDolphins = calcAverage(44, 23, 71);
+const avgKoalas = calcAverage(65, 54, 49);
+
+console.log(avgDolphins)
+console.log(avgKoalas)
+
+
+checkWinner = (dol, koa) => {
+    if (dol >= 2 * koa) {
+        console.log(`Dolphin win (${dol} vs ${koa})`)
+    } 
+    else if(koa >= 2 * dol) {
+        console.log(`Koa win (${koa} vs ${dol})`)
+    }
+   else {
+    console.log('no team wins')
+   }
+}
+
+checkWinner(10, 10)
+
+
+// challenge 2
+const calcTip = (bill) => {
+ return(bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+}
+
+const bill = [125, 555, 44]
+const tips = bill.map(calcTip)
+console.log(tips);
+
+const total = bill.map((bill, index) => bill + tips[index])
+console.log(tips)
+console.log(total)
+
+// challenge 3
--- test/index.js
+++ test/index.js
@@ -1,317 +1,317 @@
-//values and variables
-// let country = 'Kenya';
-// let continent = 'Africa'
-// let population = '10';
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+// //values and variables
+// // let country = 'Kenya';
+// // let continent = 'Africa'
+// // let population = '10';
+// // console.log(country);
+// // console.log(continent);
+// // console.log(population);
 
-// Data type
-// let isIsland = false;
-//  let language;
-//  console.log(typeof isIsland);
-//  console.log(typeof population);
-//  console.log(typeof country);
-//  console.log(typeof language);
-
-
-// //var const
-// language = 'English';
-// const country = 'United Kingdom';
-// const continent = 'Europe';
-// const population = '30 million';
-// const isIsland = false
-// isIsland = true
-
-//---------------
-//basic operator
-// console.log(population / 2)
-// population ++;
-// console.log(population)
-// console.log(population>6)
-// console.log(population < 12)
-// const descriptional1 =
-// country +
-// ' is in ' +
-// continent +
-// ', and its population is ' +
-// population +
-// '24 million people'
-// language;
-// console.log(descriptional1);
-//--------------------------
+// // Data type
+// // let isIsland = false;
+// //  let language;
+// //  console.log(typeof isIsland);
+// //  console.log(typeof population);
+// //  console.log(typeof country);
+// //  console.log(typeof language);
 
 
-// string and template Literal
-// const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`
+// // //var const
+// // language = 'English';
+// // const country = 'United Kingdom';
+// // const continent = 'Europe';
+// // const population = '30 million';
+// // const isIsland = false
+// // isIsland = true
 
-// console.log(description)
+// //---------------
+// //basic operator
+// // console.log(population / 2)
+// // population ++;
+// // console.log(population)
+// // console.log(population>6)
+// // console.log(population < 12)
+// // const descriptional1 =
+// // country +
+// // ' is in ' +
+// // continent +
+// // ', and its population is ' +
+// // population +
+// // '24 million people'
+// // language;
+// // console.log(descriptional1);
+// //--------------------------
+
+
+// // string and template Literal
+// // const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`
+
+// // console.log(description)
 
 // // if statement
-// if (population > 33) {
-//     console.log(`${country}'s populationis above average`)
-// }else {
-//     console.log(
-//         `${country}'s population is${33 - population} milloin below average`
-//     )
-// };
+// // if (population > 33) {
+// //     console.log(`${country}'s populationis above average`)
+// // }else {
+// //     console.log(
+// //         `${country}'s population is${33 - population} milloin below average`
+// //     )
+// // };
 
-// Type Conversion and Coercion
-// console.log('9' - '5');
-// console.log('19' - '13' + '17');
-// console.log('120' < 57)
-// console.log(5 + 6 + '4' + 9 - 4 - 2)
+// // Type Conversion and Coercion
+// // console.log('9' - '5');
+// // console.log('19' - '13' + '17');
+// // console.log('120' < 57)
+// // console.log(5 + 6 + '4' + 9 - 4 - 2)
 
-// equality operators
-// let numNeighbours = prompt(
-//     'how many neighbours does your country have?'
-// );
-
-
-// let answer = Number(numNeighbours)
-// console.log('Answer: ', answer)
-
-// if(answer === 1){
-//     console.log('Only 1 border')
-// }
-// else if(answer > 1){
-//     console.log('MOre than 1 border')
-// }
-// else{
-//     console.log('No border')
-// }
+// // equality operators
+// // let numNeighbours = prompt(
+// //     'how many neighbours does your country have?'
+// // );
 
 
-//logical operators
-// if (language === "English" && population < 10 && isIsland === false) {
-//     console.log(`You should live in ${country} :)`);
-//   } else {
-//     console.log(`${country} does not meet your need :(`);
-//   }
+// // let answer = Number(numNeighbours)
+// // console.log('Answer: ', answer)
+
+// // if(answer === 1){
+// //     console.log('Only 1 border')
+// // }
+// // else if(answer > 1){
+// //     console.log('MOre than 1 border')
+// // }
+// // else{
+// //     console.log('No border')
+// // }
+
+
+// //logical operators
+// // if (language === "English" && population < 10 && isIsland === false) {
+// //     console.log(`You should live in ${country} :)`);
+// //   } else {
+// //     console.log(`${country} does not meet your need :(`);
+// //   }
     
-// The Conditional (Ternary) Operator
-// console.log(
-//     `${country}'s population is ${population > 33 ? 
-//         'above' : 'below'} average`,
-// );
+// // The Conditional (Ternary) Operator
+// // console.log(
+// //     `${country}'s population is ${population > 33 ? 
+// //         'above' : 'below'} average`,
+// // );
 
-// // switch 
-// switch (language) {
-//             case 'chinese':
-//             case 'mandarin':
-//             console.log('MOST number of native speakers!');
-//             break;
-//             case 'Japanase':
-//             console.log('2nd place in number of native speakers');
-//             break;
-//             case 'english':
-//             console.log('3rd place');
-//             break;
-//             case 'hindi':
-//             console.log('Number 4');
-//             break;
-//             case 'kishwahilli':
-//             console.log('5th most spoken language');
-//             break;
-//             default:
-//             console.log('Great language too :D');
+// // // switch 
+// // switch (language) {
+// //             case 'chinese':
+// //             case 'mandarin':
+// //             console.log('MOST number of native speakers!');
+// //             break;
+// //             case 'Japanase':
+// //             console.log('2nd place in number of native speakers');
+// //             break;
+// //             case 'english':
+// //             console.log('3rd place');
+// //             break;
+// //             case 'hindi':
+// //             console.log('Number 4');
+// //             break;
+// //             case 'kishwahilli':
+// //             console.log('5th most spoken language');
+// //             break;
+// //             default:
+// //             console.log('Great language too :D');
+// //     }
+
+
+
+// //----------------------
+// //PART 2 FUNCTION
+//  //----------------------
+
+//  //function
+
+//  let country = 'Uganda'
+//  let population = '30'
+//  let continent = 'Africa'
+//  let capital = 'Kampala'
+// //  console.log(country)
+// //  console.log(population)
+// //  console.log(capital)
+// //  console.log(continent)
+
+
+//  //function//
+//  function describeCountry(country, population,capital, continent) {
+//       return `${country} has ${population} million people and its capitalCity is ${capital}`
+//  }
+
+//  const descUganda = describeCountry('Uganda', 10, 'Kampala')
+//  const descItaly = describeCountry('Italy', 20, 'Rome')
+//  const descFrance = describeCountry('France', 30, 'Paris')
+
+// //  console.log(descUganda)
+// //  console.log(descItaly)
+// //  console.log(descFrance)
+
+//  //Function Declarations vs. Expressions
+//  function percentageOfWorld1(population) {
+//   return (population / 8000) * 100;
+//   }
+//   const percentageOfWorld2 = function (population) {
+//   return (population / 8000) * 100;
+//   };
+//   const percUganda = percentageOfWorld1(1000);
+//   const percItaly = percentageOfWorld1(1200);
+//   const percFrance = percentageOfWorld1(5800);
+//   // console.log(percUganda, percFrance, percItaly);
+
+
+// //ARROW FUNCTION
+// const percentageOfWorld3 = population => (population / 8000) * 100
+// const percUganda1 = percentageOfWorld3(30)
+// const percItaly1 = percentageOfWorld3(20)
+// const percFrance1 = percentageOfWorld3(50)
+
+// // console.log(percFrance1, percItaly1, percUganda1)
+
+
+// // function calling other function
+// const describePopulation = function (country, population) {
+//   const percentage = percentageOfWorld1(population);
+//   const description = `${country} has ${population} million
+//   people, which is about ${percentage}% of the world.`;
+//   // console.log(description);
+//   };
+//   // describePopulation('Uganda', 20);
+//   // describePopulation('Italy', 2200);
+//   // describePopulation('France', 664);
+
+
+
+//   //: Introduction to Arrays
+//   const populations = [20, 2882, 664, 156]
+//   // // console.log(populations, populations.length === 4)
+//   // const percentages = [
+//   //   percentageOfWorld1(populations[0]),
+//   //   percentageOfWorld1(populations[1]),
+//   //   percentageOfWorld1(populations[2]),
+//   //   percentageOfWorld1(populations[3]),
+//   // ]
+//   // console.log(percentages)
+
+
+//   //Basic Array Operations (Methods)
+//   const neighbours = ['Rwanda', 'Burundi', 'Tanzania']
+//   neighbours. push('Kigali')
+//   // console.log(neighbours)
+
+//   // neighbours.pop()
+//   // console.log(neighbours)
+
+// // if (neighbours.includes('Tanzania')) {
+// //   console.log('probably not a country from Asia :D')
+// // }
+
+// neighbours[neighbours.indexOf('kenya')] = 'Republic of Kenya'
+// // console.log(neighbours)
+
+
+// //Introduction to Objects
+// // const myCountry = {
+// //   country: 'Tanzania',
+// //   capital: 'Dodoma',
+// //   population: 6,
+// //   language: 'Kiswahilli',
+// //   neighbours: ['Uganda', 'Kenya', 'Mozambique']
+// // }
+
+// // Dot vs. Bracket Notation
+// // console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language} - speaking people ${myCountry.neighbours.length} neighbouring countries and capital called ${capital}
+// //   `)
+
+//   // myCountry.population += 2
+//   // console.log(myCountry.population) 
+//   // myCountry.population -= 2
+//   // console.log(myCountry.population)
+
+//   //Object Methods
+//   const myCountry = {
+//     country: 'South Africa',
+//     capital: 'Cape town',
+//     language: 'english',
+//     population: 6,
+//     voter: 50,
+//     neighbours: ['Norway', 'Sweden', 'Russia'],
+//     describe: function () {
+//     console.log(
+//     `${this.country} has ${this.population} million
+//     ${this.language}-speaking people,
+//     ${this.neighbours.length} neighbouring countries and a
+//     capital called ${this.capital}.`
+//     );
+//     },
+//     checkIsland: function () {
+//     this.isIsland = this.neighbours.length === 0 ? true :
+//     false;
+//     // Even simpler version (see why this works...)
+//     // this.isIsland = !Boolean(this.neighbours.length);
 //     }
+//     };
+//     // myCountry.describe();
+//     myCountry.checkIsland();
+//     // console.log(myCountry);
+
+
+// //Iteration: FOR LOOP
+// // for (let voter = 1; voter <= 100; voter++)
+// //   console.log(`Voter number ${voter} is currently voting`);
+
+
+// // // : Looping Arrays, Breaking and Continuing
+// // const populations = [20, 2882, 662, 156];
+//  const percentages2 = [];
+//  for (let i = 0; i < populations.length; i++) {
+//  const perc = percentageOfWorld1(populations[i]);
+//  percentages2.push(perc);
+//  }
+//  console.log(percentages2)
+
+// //  const listOfNeighbours = [
+// //   ['Zambia', 'Ghana'],
+// //   ['Congo'],
+// //   ['Lybia', 'South Africa', 'Liberia'],
+// //   ];
+// //   for (let i = 0; i < listOfNeighbours.length; i++)
+// //   for (let y = 0; y < listOfNeighbours[i].length; y++)
+// //   console.log(`Neighbour: ${listOfNeighbours[i][y]}`)
 
 
 
-//----------------------
-//PART 2 FUNCTION
- //----------------------
-
- //function
-
- let country = 'Uganda'
- let population = '30'
- let continent = 'Africa'
- let capital = 'Kampala'
-//  console.log(country)
-//  console.log(population)
-//  console.log(capital)
-//  console.log(continent)
-
-
- //function//
- function describeCountry(country, population,capital, continent) {
-      return `${country} has ${population} million people and its capitalCity is ${capital}`
- }
-
- const descUganda = describeCountry('Uganda', 10, 'Kampala')
- const descItaly = describeCountry('Italy', 20, 'Rome')
- const descFrance = describeCountry('France', 30, 'Paris')
-
-//  console.log(descUganda)
-//  console.log(descItaly)
-//  console.log(descFrance)
-
- //Function Declarations vs. Expressions
- function percentageOfWorld1(population) {
-  return (population / 8000) * 100;
-  }
-  const percentageOfWorld2 = function (population) {
-  return (population / 8000) * 100;
-  };
-  const percUganda = percentageOfWorld1(1000);
-  const percItaly = percentageOfWorld1(1200);
-  const percFrance = percentageOfWorld1(5800);
-  // console.log(percUganda, percFrance, percItaly);
-
-
-//ARROW FUNCTION
-const percentageOfWorld3 = population => (population / 8000) * 100
-const percUganda1 = percentageOfWorld3(30)
-const percItaly1 = percentageOfWorld3(20)
-const percFrance1 = percentageOfWorld3(50)
-
-// console.log(percFrance1, percItaly1, percUganda1)
-
-
-// function calling other function
-const describePopulation = function (country, population) {
-  const percentage = percentageOfWorld1(population);
-  const description = `${country} has ${population} million
-  people, which is about ${percentage}% of the world.`;
-  // console.log(description);
-  };
-  // describePopulation('Uganda', 20);
-  // describePopulation('Italy', 2200);
-  // describePopulation('France', 664);
-
-
-
-  //: Introduction to Arrays
-  const populations = [20, 2882, 664, 156]
-  // // console.log(populations, populations.length === 4)
-  // const percentages = [
-  //   percentageOfWorld1(populations[0]),
-  //   percentageOfWorld1(populations[1]),
-  //   percentageOfWorld1(populations[2]),
-  //   percentageOfWorld1(populations[3]),
-  // ]
-  // console.log(percentages)
-
-
-  //Basic Array Operations (Methods)
-  const neighbours = ['Rwanda', 'Burundi', 'Tanzania']
-  neighbours. push('Kigali')
-  // console.log(neighbours)
-
-  // neighbours.pop()
-  // console.log(neighbours)
-
-// if (neighbours.includes('Tanzania')) {
-//   console.log('probably not a country from Asia :D')
-// }
-
-neighbours[neighbours.indexOf('kenya')] = 'Republic of Kenya'
-// console.log(neighbours)
-
-
-//Introduction to Objects
-// const myCountry = {
-//   country: 'Tanzania',
-//   capital: 'Dodoma',
-//   population: 6,
-//   language: 'Kiswahilli',
-//   neighbours: ['Uganda', 'Kenya', 'Mozambique']
-// }
-
-// Dot vs. Bracket Notation
-// console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language} - speaking people ${myCountry.neighbours.length} neighbouring countries and capital called ${capital}
-//   `)
-
-  // myCountry.population += 2
-  // console.log(myCountry.population) 
-  // myCountry.population -= 2
-  // console.log(myCountry.population)
-
-  //Object Methods
-  const myCountry = {
-    country: 'South Africa',
-    capital: 'Cape town',
-    language: 'english',
-    population: 6,
-    voter: 50,
-    neighbours: ['Norway', 'Sweden', 'Russia'],
-    describe: function () {
-    console.log(
-    `${this.country} has ${this.population} million
-    ${this.language}-speaking people,
-    ${this.neighbours.length} neighbouring countries and a
-    capital called ${this.capital}.`
-    );
-    },
-    checkIsland: function () {
-    this.isIsland = this.neighbours.length === 0 ? true :
-    false;
-    // Even simpler version (see why this works...)
-    // this.isIsland = !Boolean(this.neighbours.length);
-    }
-    };
-    // myCountry.describe();
-    myCountry.checkIsland();
-    // console.log(myCountry);
-
-
-//Iteration: FOR LOOP
-// for (let voter = 1; voter <= 100; voter++)
-//   console.log(`Voter number ${voter} is currently voting`);
-
-
-// // : Looping Arrays, Breaking and Continuing
-// const populations = [20, 2882, 662, 156];
- const percentages2 = [];
- for (let i = 0; i < populations.length; i++) {
- const perc = percentageOfWorld1(populations[i]);
- percentages2.push(perc);
- }
- console.log(percentages2)
-
-//  const listOfNeighbours = [
-//   ['Zambia', 'Ghana'],
-//   ['Congo'],
-//   ['Lybia', 'South Africa', 'Liberia'],
+//   //Looping Backwards and Loops in Loop
+//   const listOfNeighbours = [
+//     ['Zambia', 'Ghana', 'Congo', 'Lybia', 'South Africa', 'Liberia']
 //   ];
-//   for (let i = 0; i < listOfNeighbours.length; i++)
-//   for (let y = 0; y < listOfNeighbours[i].length; y++)
-//   console.log(`Neighbour: ${listOfNeighbours[i][y]}`)
-
-
-
-  //Looping Backwards and Loops in Loop
-  const listOfNeighbours = [
-    ['Zambia', 'Ghana', 'Congo', 'Lybia', 'South Africa', 'Liberia']
-  ];
-for (let i = 0; i < listOfNeighbours.length; i++) {
-    for (let y = listOfNeighbours[i].length - 1; y >= 0; y--) {
-        console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
-    }
-}
-
-
-
-//  //The while Loop
-const percentage3 = [];
-let i = 0
-
-console.log(percentage3)
-
-while(i < population.length){
-  console.log(populations)
-  const perc = percentageOfWorld1(populations[i])
-  percentage3.push(perc)
-  i++
-}
-console.log(percentage3)
-// while (1 < population?.length) {
-//   // console.log(population)
-//   const perc = percentageOfWorld1(populations[i]);
-//   percentage3.push(perc);
-//   i++;
+// for (let i = 0; i < listOfNeighbours.length; i++) {
+//     for (let y = listOfNeighbours[i].length - 1; y >= 0; y--) {
+//         console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
+//     }
 // }
+
+
+
+// //  //The while Loop
+// const percentage3 = [];
+// let i = 0
 
 // console.log(percentage3)
+
+// while(i < population.length){
+//   console.log(populations)
+//   const perc = percentageOfWorld1(populations[i])
+//   percentage3.push(perc)
+//   i++
+// }
+// console.log(percentage3)
+// // while (1 < population?.length) {
+// //   // console.log(population)
+// //   const perc = percentageOfWorld1(populations[i]);
+// //   percentage3.push(perc);
+// //   i++;
+// // }
+
+// // console.log(percentage3)
